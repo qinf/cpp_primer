d0eb6f1b773ac5d06a809b2829ac02bcba05db04
JunZhong Qin <qinjunzhong@gmail.com>
2015-01-07 09:00:10 -0500

    add X.h Y.h

---
+++ Chapter7_Class/X.h
@@ -0,0 +1,10 @@
+#ifndef _x_h
+#define _x_h
+
+class Y;
+class X {
+private:
+    Y *_y;
+};
+
+#endif
---
+++ Chapter7_Class/Y.h
@@ -0,0 +1,9 @@
+#ifndef _y_h
+#define _y_h
+#include "X.h"
+class Y {
+    private:
+        X _x;
+};
+
+#endif
