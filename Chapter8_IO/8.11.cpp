ea9b1891b93aabb4df3a6a0f867ed4ce013797b8
JunZhong Qin <qinjunzhong@gmail.com>
2015-01-27 11:01:47 -0500

    move the istringstream obj out of the while loop

--- Chapter8_IO/8.11.cpp
+++ Chapter8_IO/8.11.cpp
@@ -10,16 +10,29 @@ struct PersonInfo {
     vector<string> phones;
 };
 
+void print(vector<PersonInfo> &);
 void func() {
     string line, word;
     vector<PersonInfo> people;
+    istringstream record;
     while(getline(cin, line)) {
         PersonInfo info;
-        istringstream record(line);
+        record.clear();
+        record.str(line);
         record >> info.name;
         while(record >> word)
             info.phones.push_back(word);
         people.push_back(info);
+    }
+    print(people);
+}
+
+void print(vector<PersonInfo> &infos) {
+    for (auto &info : infos) {
+        cout << "name:" << info.name << " phones:";
+        for (auto &s : info.phones)
+            cout << s << " ";
+        cout << endl;
     }
 }
 
