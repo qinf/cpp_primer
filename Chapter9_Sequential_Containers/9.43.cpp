4ffd4886d556bff8a46d75ef69d14210fbd45bd1
JunZhong Qin <qinjunzhong@gmail.com>
2015-02-02 10:07:07 -0500

    add 9.43.cpp

---
+++ Chapter9_Sequential_Containers/9.43.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using std::string; using std::cout; using std::endl;
+
+void replace(string &s, string oldVal, string newVal) {
+    for (auto index = 0; index != s.size(); ++index) {
+        if (s.substr(index, oldVal.size()) == oldVal) {
+            s.erase(index, oldVal.size());
+            s.insert(index, newVal);
+        }
+    }
+}
+int main() {
+    string s("tho hah");
+    replace(s, "tho", "though");
+    cout << s << endl;
+    s = "thru bibi";
+    replace(s, "thru", "through");
+    cout << s << endl;
+}
