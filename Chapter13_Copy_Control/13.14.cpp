01267f9e51fa5b090ecb8cf73e9f690849a24247
JunZhong Qin <qinjunzhong@gmail.com>
2015-02-05 10:17:56 -0500

    add 13.14.cpp

---
+++ Chapter13_Copy_Control/13.14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using std::cout; using std::endl;
+
+class numbered {
+public:
+    numbered() { ++mysn; }
+    friend void f(numbered s) { cout << s.mysn << endl; }
+private:
+    static int mysn;
+};
+
+int numbered::mysn  = 0;
+
+int main() {
+    numbered a, b = a, c = b;
+    f(a);
+    f(b);
+    f(c);
+}
