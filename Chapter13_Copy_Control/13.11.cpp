3486fdf796497fff8bf55a9b94ac9f8c2ef0880b
JunZhong Qin <qinjunzhong@gmail.com>
2015-02-05 09:47:27 -0500

    add 13.11.cpp

---
+++ Chapter13_Copy_Control/13.11.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+using std::ostream; using std::cout; using std::endl;
+using std::string;
+
+class HasPtr {
+public:
+    HasPtr(const string &s = string()):
+        ps(new string(s)), i(0) { }
+    HasPtr(const HasPtr &ptr): ps(new string(*(ptr.ps))), i(ptr.i) { }
+    HasPtr& operator=(const HasPtr &p) {
+        *ps = *(p.ps);
+        i = p.i;
+        return *this;
+    }
+    ~HasPtr() {
+        delete ps;
+    }
+    friend ostream& print(ostream &os, HasPtr &p) {
+        os << p.ps << " " << p.i << endl;
+        return os;
+    }
+
+private:
+    string *ps;
+    int i;
+};
+
+int main() {
+    HasPtr p("I am a dog");
+    print(cout, p);
+    HasPtr p1 = p;
+    print(cout, p1);
+    return 0;
+}
