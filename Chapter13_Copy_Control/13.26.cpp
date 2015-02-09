3937ba0a963cabc276e57cc74efc81d25a050767
JunZhong Qin <qinjunzhong@gmail.com>
2015-02-09 02:34:03 -0500

    move the definition of StrBlob::begin() and StrBlob::end() after the definition of class StrBlobPtr

--- Chapter13_Copy_Control/13.26.cpp
+++ Chapter13_Copy_Control/13.26.cpp
@@ -17,27 +17,32 @@ class StrBlob {
 public:
     using size_type = vector<string>::size_type;
     StrBlob(): data(shared_ptr<vector<string>>()) { }
-    StrBlob(initializer_list<string> il): data(make_shared<vector<string>>(il)) { }
+    //StrBlob(initializer_list<string> il): data(make_shared<vector<string>>(il)) { }
+    StrBlob(initializer_list<string> il);
     StrBlob(const StrBlob &b);
     StrBlob& operator = (const StrBlob &b);
     size_type size() const { return data->size(); }
-    StrBlobPtr begin() { return StrBlobPtr(*this); }
-    StrBlobPtr end() {
-        auto ret = StrBlobPtr(*this, data->size());
-        return ret;
-    }
+    //StrBlobPtr begin() { return StrBlobPtr(*this); }
+    //StrBlobPtr end() {
+    //    auto ret = StrBlobPtr(*this, data->size());
+    //    return ret;
+    //}
     bool empty() { return data->empty(); }
     void push_back(string &s) { data->push_back(s); }
     void pop_back();
     // visiting item
     string& front();
     string& back();
+    StrBlobPtr begin();
+    StrBlobPtr end();
 private:
     shared_ptr<vector<string>> data;
     void check(size_type i, const string &msg) const;
 };
 //copy constructor
 inline StrBlob::StrBlob(const StrBlob &b): data(make_shared<vector<string>>(*b.data)) { }
+StrBlob::StrBlob(initializer_list<string> il): data(make_shared<vector<string>>(il)) { }
+
 // copy-assignment operator
 inline StrBlob& StrBlob::operator = (const StrBlob &b) {
     auto p = make_shared<vector<string>>(*b.data);
@@ -90,5 +95,12 @@ StrBlobPtr& StrBlobPtr::incr() {
     check(curr, "increment past end of StrBlobPtr");
     ++curr;
     return *this;
+}
+inline StrBlobPtr StrBlob::begin() {
+    return StrBlobPtr(*this);
+}
+inline StrBlobPtr StrBlob::end() {
+    auto ret = StrBlobPtr(*this, data->size());
+    return ret;
 }
 #endif
